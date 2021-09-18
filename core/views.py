13fcd5e6665fdee2ab1d0c8fadb19c973a13a0e1
Ankitji09 <64421490+Ankitji09@users.noreply.github.com>
2021-09-18 18:58:30 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 class APIList(APIView):
     """
